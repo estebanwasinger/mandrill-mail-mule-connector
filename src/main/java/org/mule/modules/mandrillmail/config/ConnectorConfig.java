d10e01022eb055ff05cfae3229c0c4618d3f43bd
estebanwasinger <estebanwasinger@gmail.com>
2015-10-03 20:02:13 -0300

    Improved error handling in the test connectivity

--- src/main/java/org/mule/modules/mandrillmail/config/ConnectorConfig.java
+++ src/main/java/org/mule/modules/mandrillmail/config/ConnectorConfig.java
@@ -1,26 +1,38 @@
 package org.mule.modules.mandrillmail.config;
 
 import com.microtripit.mandrillapp.lutung.MandrillApi;
+import com.microtripit.mandrillapp.lutung.model.MandrillApiError;
 import org.mule.api.ConnectionException;
 import org.mule.api.ConnectionExceptionCode;
 import org.mule.api.annotations.Configurable;
 import org.mule.api.annotations.TestConnectivity;
 import org.mule.api.annotations.components.Configuration;
 
+import java.io.IOException;
+
 @Configuration(friendlyName = "API Token Configuration")
 public class ConnectorConfig {
 
-    MandrillApi mandrillApi;
+    private String NULL_API_KEY_ERROR_MSG = "API Key is null, please fill it with a valid value";
+
+    private MandrillApi mandrillApi;
 
     @Configurable
     String apiKey;
 
     @TestConnectivity
     public void connect() throws ConnectionException {
+        if (apiKey == null) {
+            throw new ConnectionException(ConnectionExceptionCode.INCORRECT_CREDENTIALS, NULL_API_KEY_ERROR_MSG, NULL_API_KEY_ERROR_MSG);
+        }
+
         MandrillApi mandrillApi = new MandrillApi(apiKey);
+
         try {
             mandrillApi.users().ping();
-        } catch (Exception e) {
+        } catch (MandrillApiError e) {
+            throw new ConnectionException(ConnectionExceptionCode.INCORRECT_CREDENTIALS, e.getMandrillErrorMessage(), e.getMandrillErrorMessage());
+        } catch (IOException e) {
             throw new ConnectionException(ConnectionExceptionCode.UNKNOWN, e.getMessage(), e.getMessage());
         }
     }
